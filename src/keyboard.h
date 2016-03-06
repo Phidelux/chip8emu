bab51906246fae31aba629ed94496f79c7553ea7
Bornageek <info@bornageek.com>
2016-03-06 13:01:23 +0100

    Added SDL key definitions for emulator specific actions..

--- src/keyboard.h
+++ src/keyboard.h
@@ -20,7 +20,7 @@ public:
    void reset();
 
    bool isKeyDown(std::uint8_t key);
-   
+
    void setQuitHandler(std::function<void()> quitHandler);
 
 private:
@@ -28,14 +28,14 @@ private:
 
    std::vector<bool> mKeyPad;
    const std::vector<SDL_Keycode> mPadMap {
-      SDLK_x, SDLK_1, SDLK_2, SDLK_3, 
-      SDLK_q, SDLK_w, SDLK_e, SDLK_a, 
-      SDLK_s, SDLK_d, SDLK_y, SDLK_c, 
+      SDLK_x, SDLK_1, SDLK_2, SDLK_3,
+      SDLK_q, SDLK_w, SDLK_e, SDLK_a,
+      SDLK_s, SDLK_d, SDLK_y, SDLK_c,
       SDLK_4, SDLK_r, SDLK_f, SDLK_v
    };
 
    const std::vector<SDL_Keycode> mEmuMap {
-      
+      SDLK_ESCAPE, SDLK_F8, SDLK_F9, SDLK_F10, SDLK_SPACE
    };
 };
 
